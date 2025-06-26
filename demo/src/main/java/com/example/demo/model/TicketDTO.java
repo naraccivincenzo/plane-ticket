e76c18afc73ed64f1ef5b3cd57417e22f2143056
naracci.vincenzo <143946955+naraccivincenzo@users.noreply.github.com>
2025-06-26 12:58:20 +0200

    Update TicketDTO.java

--- demo/src/main/java/com/example/demo/model/TicketDTO.java
+++ demo/src/main/java/com/example/demo/model/TicketDTO.java
@@ -4,29 +4,17 @@ import java.util.List;
 
 public class TicketDTO {
     private List<Passenger> passengers;
-    private String route;
-    private String departureTime;
-    private String arrivalTime;
-    private String airlineCode;
-    private double price;
+    private List<FlightDTO> flights;
     private String rules;
-    private String pnr;
+    private boolean samePnr;
 
     // Getters e Setters
     public List<Passenger> getPassengers() { return passengers; }
     public void setPassengers(List<Passenger> passengers) { this.passengers = passengers; }
-    public String getRoute() { return route; }
-    public void setRoute(String route) { this.route = route; }
-    public String getDepartureTime() { return departureTime; }
-    public void setDepartureTime(String departureTime) { this.departureTime = departureTime; }
-    public String getArrivalTime() { return arrivalTime; }
-    public void setArrivalTime(String arrivalTime) { this.arrivalTime = arrivalTime; }
-    public String getAirlineCode() { return airlineCode; }
-    public void setAirlineCode(String airlineCode) { this.airlineCode = airlineCode; }
-    public double getPrice() { return price; }
-    public void setPrice(double price) { this.price = price; }
+    public List<FlightDTO> getFlights() { return flights; }
+    public void setFlights(List<FlightDTO> flights) { this.flights = flights; }
     public String getRules() { return rules; }
     public void setRules(String rules) { this.rules = rules; }
-    public String getPnr() { return pnr; }
-    public void setPnr(String pnr) { this.pnr = pnr; }
+    public boolean isSamePnr() { return samePnr; }
+    public void setSamePnr(boolean samePnr) { this.samePnr = samePnr; }
 }
